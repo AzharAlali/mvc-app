57e7cf8ea26148436e58706ca66c7f4a5eae092f
jeevan mure <jeevanreddymure@gmail.com>
2019-11-06 15:30:27 -0600

    added

--- routes/router.js
+++ routes/router.js
@@ -26,7 +26,7 @@ router.get('/index', (req, res, next) => {
 // Defer path requests to a particular controller
 router.use('/dev', require('../controllers/developer.js'))
 router.use('/ins', require('../controllers/instructor'))
-router.use('/cou',require('../controllers/course.js'))
+router.use('/cou', require('../controllers/course'))
 
 console.log('END routing')
 module.exports = router
