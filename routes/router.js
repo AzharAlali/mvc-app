b9ae3ce6fc5d192e5d35759f6c958ebf8bcaa180
jeevan mure <jeevanreddymure@gmail.com>
2019-11-06 22:38:14 -0600

    added

--- routes/router.js
+++ routes/router.js
@@ -22,6 +22,7 @@ router.get('/index', (req, res, next) => {
 
 // Route requests that start with '/dev' to a particular controller
 router.use('/student', require('../controllers/student.js'))
+router.use('/course',require('../controllers/course'))
 
 // catch 404 and forward to error handler
 router.use((req, res, next) => {
