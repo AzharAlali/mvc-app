43eed6305f52e3f396724a8eda52897d7568b6c2
Kamal Reddy <kamalreddy4195gmail.com>
2019-11-11 12:05:00 -0600

    updated repo

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,5 +1,5 @@
 const Datastore = require('nedb') // set up a temporary (in memory) database
-const developerData = require('../data/developers.json') // read in data file
+
 const studentsData = require('../data/students.json')
 const coursesData = require('../data/courses.json')
 const sectionsData = require('../data/sections.json')
@@ -16,14 +16,14 @@ module.exports = (app) => {
   const instructordb={}
 
   
-  db.developers = new Datastore() // new object property
+  
 
   studentdb.students=new Datastore()
   coursedb.courses=new Datastore()
   sectiondb.sections=new Datastore()
   instructordb.instructors=new Datastore()
 
-  db.developers.loadDatabase() // call the loadDatabase method
+  
   studentdb.students.loadDatabase()
   coursedb.courses.loadDatabase()
   sectiondb.sections.loadDatabase()
@@ -34,16 +34,15 @@ module.exports = (app) => {
   coursedb.courses.insert(coursesData)
   sectiondb.sections.insert(sectionsData)
   instructordb.instructors.insert(instructorsData)
-  db.developers.insert(developerData)
+  
 
   // initialize app.locals (these objects are available to the controllers)
   app.locals.students=studentdb.students.find(studentsData)
   app.locals.courses=coursedb.courses.find(coursesData)
   app.locals.sections=sectiondb.sections.find(sectionsData)
   app.locals.instructors=instructordb.instructors.find(instructorsData)
-  app.locals.developers = db.developers.find(developerData)
-
-  console.log(`${app.locals.developers.query.length} developers seeded`)
+  
+  
   console.log(`${app.locals.students.query.length} students seeded`)
   console.log(`${app.locals.sections.query.length} sections seeded`)
   console.log(`${app.locals.courses.query.length} courses seeded`)
